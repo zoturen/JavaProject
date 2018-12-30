0ae9eb13e1fefb88ee21ff82a5e6f90d566711d6
Zoturen <chrillejansson@me.com>
2018-12-30 17:34:49 +0100

    Hello worlded our project!

---
+++ src/com/gui/MastermindGUI.java
@@ -0,0 +1,35 @@
+/*
+ * Created by JFormDesigner on Sun Dec 30 17:31:33 CET 2018
+ */
+
+package com.gui;
+
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ * @author unknown
+ */
+public class MastermindGUI extends JFrame {
+    public MastermindGUI() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        // Generated using JFormDesigner Evaluation license - unknown
+
+        //======== this ========
+        Container contentPane = getContentPane();
+        contentPane.setLayout(null);
+
+        contentPane.setPreferredSize(new Dimension(400, 300));
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // Generated using JFormDesigner Evaluation license - unknown
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
