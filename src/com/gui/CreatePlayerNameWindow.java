ee84edb0d14452ffcc157abd03d4c4c7ed3cc2b3
Snaldo <hard_asfalt@hotmail.com>
2018-12-31 01:40:19 +0100

    added dot graphics

---
+++ src/com/gui/CreatePlayerNameWindow.java
@@ -0,0 +1,94 @@
+/*
+ * Created by JFormDesigner on Sun Dec 30 22:54:48 CET 2018
+ */
+
+package com.gui;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/**
+ * @author Marcus
+ */
+public class CreatePlayerNameWindow extends JFrame {
+    public CreatePlayerNameWindow() {
+        initComponents();
+    }
+
+    private void button1ActionPerformed(ActionEvent e) {
+        // TODO add your code here
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        // Generated using JFormDesigner Evaluation license - Marcus
+        button1 = new JButton();
+        label1 = new JLabel();
+        textField1 = new JTextField();
+        label2 = new JLabel();
+        label3 = new JLabel();
+        comboBox1 = new JComboBox<>();
+        label4 = new JLabel();
+
+        //======== this ========
+        setResizable(false);
+        Container contentPane = getContentPane();
+        contentPane.setLayout(null);
+
+        //---- button1 ----
+        button1.setText("Confirm");
+        button1.addActionListener(e -> button1ActionPerformed(e));
+        contentPane.add(button1);
+        button1.setBounds(new Rectangle(new Point(205, 475), button1.getPreferredSize()));
+
+        //---- label1 ----
+        label1.setText("Choose a Player Name");
+        contentPane.add(label1);
+        label1.setBounds(185, 395, 150, 30);
+        contentPane.add(textField1);
+        textField1.setBounds(185, 425, 125, textField1.getPreferredSize().height);
+
+        //---- label2 ----
+        label2.setText("Mastermind");
+        label2.setFont(new Font("Segoe UI", Font.PLAIN, 30));
+        contentPane.add(label2);
+        label2.setBounds(160, 15, 170, 35);
+
+        //---- label3 ----
+        label3.setText("Choose Difficulty Level");
+        label3.setFont(new Font("Segoe UI", Font.PLAIN, 18));
+        contentPane.add(label3);
+        label3.setBounds(145, 90, 195, 55);
+
+        //---- comboBox1 ----
+        comboBox1.setModel(new DefaultComboBoxModel<>(new String[] {
+            "Easy",
+            "Normal",
+            "Hard"
+        }));
+        contentPane.add(comboBox1);
+        comboBox1.setBounds(new Rectangle(new Point(180, 145), comboBox1.getPreferredSize()));
+
+        //---- label4 ----
+        label4.setText("Rules explenation...");
+        contentPane.add(label4);
+        label4.setBounds(95, 220, 275, 120);
+
+        contentPane.setPreferredSize(new Dimension(490, 560));
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // Generated using JFormDesigner Evaluation license - Marcus
+    private JButton button1;
+    private JLabel label1;
+    private JTextField textField1;
+    private JLabel label2;
+    private JLabel label3;
+    private JComboBox<String> comboBox1;
+    private JLabel label4;
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
--- src/com/gui/MastermindGUI.java
+++ src/com/gui/MastermindGUI.java
@@ -17,19 +17,176 @@ public class MastermindGUI extends JFrame {
 
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
-        // Generated using JFormDesigner Evaluation license - unknown
+        // Generated using JFormDesigner Evaluation license - Marcus
+        label2 = new JLabel();
+        label1 = new JLabel();
+        label3 = new JLabel();
+        label4 = new JLabel();
+        label5 = new JLabel();
+        button2 = new JButton();
+        button3 = new JButton();
+        button4 = new JButton();
+        button5 = new JButton();
+        button6 = new JButton();
+        button7 = new JButton();
+        label6 = new JLabel();
+        button8 = new JButton();
+        label7 = new JLabel();
 
         //======== this ========
         Container contentPane = getContentPane();
-        contentPane.setLayout(null);
 
-        contentPane.setPreferredSize(new Dimension(400, 300));
+        //---- label2 ----
+        label2.setText("Mastermind");
+        label2.setFont(new Font("Segoe UI", Font.PLAIN, 30));
+
+        //---- label1 ----
+        label1.setText("?");
+        label1.setFont(new Font("Segoe UI", Font.BOLD, 48));
+
+        //---- label3 ----
+        label3.setText("?");
+        label3.setFont(new Font("Segoe UI", Font.BOLD, 48));
+
+        //---- label4 ----
+        label4.setText("?");
+        label4.setFont(new Font("Segoe UI", Font.BOLD, 48));
+
+        //---- label5 ----
+        label5.setText("?");
+        label5.setFont(new Font("Segoe UI", Font.BOLD, 48));
+
+        //---- button2 ----
+        button2.setText("Black");
+        button2.setForeground(Color.black);
+        button2.setFont(new Font("Segoe UI", Font.BOLD, 20));
+
+        //---- button3 ----
+        button3.setText("White");
+        button3.setForeground(Color.white);
+        button3.setFont(new Font("Segoe UI", Font.BOLD, 20));
+
+        //---- button4 ----
+        button4.setText("Red");
+        button4.setForeground(Color.red);
+        button4.setFont(new Font("Segoe UI", Font.BOLD, 20));
+
+        //---- button5 ----
+        button5.setText("Purple");
+        button5.setForeground(new Color(153, 0, 153));
+        button5.setFont(new Font("Segoe UI", Font.BOLD, 20));
+
+        //---- button6 ----
+        button6.setText("Green");
+        button6.setForeground(new Color(0, 153, 0));
+        button6.setFont(new Font("Segoe UI", Font.BOLD, 20));
+
+        //---- button7 ----
+        button7.setText("Blue");
+        button7.setForeground(new Color(0, 153, 204));
+        button7.setFont(new Font("Segoe UI", Font.BOLD, 20));
+
+        //---- label6 ----
+        label6.setText("Pick Your Colors");
+        label6.setFont(new Font("Segoe UI", Font.PLAIN, 20));
+
+        //---- button8 ----
+        button8.setText("Evaluate");
+        button8.setFont(new Font("Segoe UI", Font.BOLD, 20));
+        button8.setBackground(new Color(204, 204, 204));
+
+        GroupLayout contentPaneLayout = new GroupLayout(contentPane);
+        contentPane.setLayout(contentPaneLayout);
+        contentPaneLayout.setHorizontalGroup(
+            contentPaneLayout.createParallelGroup()
+                .addGroup(contentPaneLayout.createSequentialGroup()
+                    .addGroup(contentPaneLayout.createParallelGroup()
+                        .addGroup(contentPaneLayout.createSequentialGroup()
+                            .addGap(224, 224, 224)
+                            .addGroup(contentPaneLayout.createParallelGroup()
+                                .addComponent(label2, GroupLayout.PREFERRED_SIZE, 170, GroupLayout.PREFERRED_SIZE)
+                                .addGroup(contentPaneLayout.createSequentialGroup()
+                                    .addGap(26, 26, 26)
+                                    .addComponent(label7, GroupLayout.PREFERRED_SIZE, 99, GroupLayout.PREFERRED_SIZE))))
+                        .addGroup(contentPaneLayout.createSequentialGroup()
+                            .addGap(215, 215, 215)
+                            .addComponent(button8, GroupLayout.PREFERRED_SIZE, 180, GroupLayout.PREFERRED_SIZE)))
+                    .addContainerGap(218, Short.MAX_VALUE))
+                .addGroup(GroupLayout.Alignment.TRAILING, contentPaneLayout.createSequentialGroup()
+                    .addContainerGap(84, Short.MAX_VALUE)
+                    .addGroup(contentPaneLayout.createParallelGroup()
+                        .addGroup(GroupLayout.Alignment.TRAILING, contentPaneLayout.createSequentialGroup()
+                            .addGroup(contentPaneLayout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+                                .addComponent(label6)
+                                .addGroup(contentPaneLayout.createSequentialGroup()
+                                    .addComponent(button7, GroupLayout.PREFERRED_SIZE, 70, GroupLayout.PREFERRED_SIZE)
+                                    .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                                    .addComponent(button6, GroupLayout.PREFERRED_SIZE, 70, GroupLayout.PREFERRED_SIZE)
+                                    .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                                    .addComponent(button5, GroupLayout.PREFERRED_SIZE, 70, GroupLayout.PREFERRED_SIZE)
+                                    .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                                    .addComponent(button4, GroupLayout.PREFERRED_SIZE, 70, GroupLayout.PREFERRED_SIZE)))
+                            .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                            .addComponent(button2, GroupLayout.PREFERRED_SIZE, 70, GroupLayout.PREFERRED_SIZE)
+                            .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                            .addComponent(button3, GroupLayout.PREFERRED_SIZE, 70, GroupLayout.PREFERRED_SIZE)
+                            .addGap(79, 79, 79))
+                        .addGroup(GroupLayout.Alignment.TRAILING, contentPaneLayout.createSequentialGroup()
+                            .addComponent(label1, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE)
+                            .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                            .addComponent(label3, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE)
+                            .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                            .addComponent(label4, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE)
+                            .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                            .addComponent(label5, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE)
+                            .addGap(200, 200, 200))))
+        );
+        contentPaneLayout.setVerticalGroup(
+            contentPaneLayout.createParallelGroup()
+                .addGroup(contentPaneLayout.createSequentialGroup()
+                    .addComponent(label2, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
+                    .addGap(50, 50, 50)
+                    .addGroup(contentPaneLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                        .addComponent(label1, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE)
+                        .addComponent(label3, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE)
+                        .addComponent(label4, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE)
+                        .addComponent(label5, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE))
+                    .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 383, Short.MAX_VALUE)
+                    .addComponent(label7, GroupLayout.PREFERRED_SIZE, 72, GroupLayout.PREFERRED_SIZE)
+                    .addGap(101, 101, 101)
+                    .addComponent(label6, GroupLayout.PREFERRED_SIZE, 34, GroupLayout.PREFERRED_SIZE)
+                    .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+                    .addGroup(contentPaneLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                        .addComponent(button7, GroupLayout.PREFERRED_SIZE, 65, GroupLayout.PREFERRED_SIZE)
+                        .addComponent(button6, GroupLayout.PREFERRED_SIZE, 65, GroupLayout.PREFERRED_SIZE)
+                        .addComponent(button5, GroupLayout.PREFERRED_SIZE, 65, GroupLayout.PREFERRED_SIZE)
+                        .addComponent(button4, GroupLayout.PREFERRED_SIZE, 65, GroupLayout.PREFERRED_SIZE)
+                        .addComponent(button2, GroupLayout.PREFERRED_SIZE, 65, GroupLayout.PREFERRED_SIZE)
+                        .addComponent(button3, GroupLayout.PREFERRED_SIZE, 65, GroupLayout.PREFERRED_SIZE))
+                    .addGap(18, 18, 18)
+                    .addComponent(button8, GroupLayout.PREFERRED_SIZE, 49, GroupLayout.PREFERRED_SIZE)
+                    .addGap(14, 14, 14))
+        );
         pack();
         setLocationRelativeTo(getOwner());
         // JFormDesigner - End of component initialization  //GEN-END:initComponents
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
-    // Generated using JFormDesigner Evaluation license - unknown
+    // Generated using JFormDesigner Evaluation license - Marcus
+    private JLabel label2;
+    private JLabel label1;
+    private JLabel label3;
+    private JLabel label4;
+    private JLabel label5;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JLabel label6;
+    private JButton button8;
+    private JLabel label7;
     // JFormDesigner - End of variables declaration  //GEN-END:variables
 }
