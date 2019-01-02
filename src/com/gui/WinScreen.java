9b234d54fe882a20906549b8c87ff454485385c2
Snaldo <hard_asfalt@hotmail.com>
2019-01-02 18:14:16 +0100

    added stuff

---
+++ src/com/gui/WinScreen.java
@@ -0,0 +1,90 @@
+/*
+ * Created by JFormDesigner on Wed Jan 02 18:03:54 CET 2019
+ */
+
+package com.gui;
+
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.GroupLayout;
+
+/**
+ * @author Marcus
+ */
+public class WinScreen extends JFrame {
+    public WinScreen() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        // Generated using JFormDesigner Evaluation license - Marcus
+        label1 = new JLabel();
+        label2 = new JLabel();
+        button1 = new JButton();
+        button2 = new JButton();
+
+        //======== this ========
+        Container contentPane = getContentPane();
+
+        //---- label1 ----
+        label1.setText("Congratulations!");
+        label1.setFont(new Font("Segoe UI", Font.PLAIN, 30));
+        label1.setHorizontalAlignment(SwingConstants.CENTER);
+
+        //---- label2 ----
+        label2.setText("You Win!");
+        label2.setFont(new Font("Segoe UI", Font.PLAIN, 22));
+        label2.setHorizontalAlignment(SwingConstants.CENTER);
+
+        //---- button1 ----
+        button1.setText("Go Agian!");
+
+        //---- button2 ----
+        button2.setText("Exit Game");
+
+        GroupLayout contentPaneLayout = new GroupLayout(contentPane);
+        contentPane.setLayout(contentPaneLayout);
+        contentPaneLayout.setHorizontalGroup(
+            contentPaneLayout.createParallelGroup()
+                .addGroup(contentPaneLayout.createSequentialGroup()
+                    .addContainerGap(73, Short.MAX_VALUE)
+                    .addGroup(contentPaneLayout.createParallelGroup()
+                        .addGroup(GroupLayout.Alignment.TRAILING, contentPaneLayout.createSequentialGroup()
+                            .addComponent(label1, GroupLayout.PREFERRED_SIZE, 265, GroupLayout.PREFERRED_SIZE)
+                            .addGap(60, 60, 60))
+                        .addGroup(GroupLayout.Alignment.TRAILING, contentPaneLayout.createSequentialGroup()
+                            .addComponent(label2, GroupLayout.PREFERRED_SIZE, 177, GroupLayout.PREFERRED_SIZE)
+                            .addGap(107, 107, 107))
+                        .addGroup(GroupLayout.Alignment.TRAILING, contentPaneLayout.createSequentialGroup()
+                            .addComponent(button1, GroupLayout.PREFERRED_SIZE, 104, GroupLayout.PREFERRED_SIZE)
+                            .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                            .addComponent(button2, GroupLayout.PREFERRED_SIZE, 104, GroupLayout.PREFERRED_SIZE)
+                            .addGap(85, 85, 85))))
+        );
+        contentPaneLayout.setVerticalGroup(
+            contentPaneLayout.createParallelGroup()
+                .addGroup(contentPaneLayout.createSequentialGroup()
+                    .addGap(40, 40, 40)
+                    .addComponent(label1, GroupLayout.PREFERRED_SIZE, 33, GroupLayout.PREFERRED_SIZE)
+                    .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                    .addComponent(label2, GroupLayout.PREFERRED_SIZE, 36, GroupLayout.PREFERRED_SIZE)
+                    .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 104, Short.MAX_VALUE)
+                    .addGroup(contentPaneLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                        .addComponent(button1)
+                        .addComponent(button2))
+                    .addGap(19, 19, 19))
+        );
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // Generated using JFormDesigner Evaluation license - Marcus
+    private JLabel label1;
+    private JLabel label2;
+    private JButton button1;
+    private JButton button2;
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
